ef9c73b481125d845850b39007d71c8ef6aab50e
Paia Thao <paiathao09@gmail.com>
2018-08-04 17:20:56 -0500

    add saveComments function when on Success view

--- server/router/feedback.router.js
+++ server/router/feedback.router.js
@@ -4,20 +4,20 @@ const pool = require('../modules/pool.js')
 
 router.get('/', (req, res) => {
   pool.query(`SELECT * FROM "feedback";`)
-  .then((results) => {
-    res.send(results.rows)
-  })
-  .catch((error) => {
-    console.log("error!", error);
-    res.sendStatus(500);
-  })
+    .then((results) => {
+      res.send(results.rows)
+    })
+    .catch((error) => {
+      console.log("error!", error);
+      res.sendStatus(500);
+    })
 });
 
 router.post('/', (req, res) => {
   const feedback = req.body;
   pool.query(`INSERT INTO "feedback" ("feeling", "understanding", "support", "comments")
-                VALUES ($1, $2, $3, $4);`, 
-                [feedback.feeling, feedback.understanding, feedback.support, feedback.comments])
+                VALUES ($1, $2, $3, $4);`,
+    [feedback.feeling, feedback.understanding, feedback.support, feedback.comments])
     .then((results) => {
       res.sendStatus(200);
     })
--- src/components/Success/Success.jsx
+++ src/components/Success/Success.jsx
@@ -1,6 +1,25 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import axios from 'axios'
 
 class Success extends Component {
+
+  componentDidMount() {
+    console.log('Component mounted')
+    this.saveComments();
+  }
+
+  saveComments = () => {
+    let feedback = this.props.newFeedback
+    axios.post('/feedback', feedback)
+    .then((response) => {
+      console.log(response.data)
+    })
+    .catch((err) => {
+      console.log('error', err)
+    });
+  }
+
   render() {
     return (
       <div>
@@ -10,4 +29,9 @@ class Success extends Component {
   }
 }
 
-export default Success;
+const mapStateToProps = (state) => {
+  return {
+    newFeedback : state.newFeedback}
+}
+
+export default connect(mapStateToProps)(Success);
